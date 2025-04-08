53241811cc18564862bda7a87997f5a984ca6a8a
tendryAxel <hei.tendry@gmail.com>
2025-04-08 09:19:46 +0300

    feat: case of Bake calcule

--- src/main/java/school/hei/patrimoine/cas/CasSetSupplier.java
+++ src/main/java/school/hei/patrimoine/cas/CasSetSupplier.java
@@ -1,10 +1,21 @@
 package school.hei.patrimoine.cas;
 
+import static java.time.temporal.TemporalAdjusters.lastDayOfYear;
+import static school.hei.patrimoine.modele.Argent.ariary;
+
+import java.time.LocalDate;
+import java.util.Set;
 import java.util.function.Supplier;
+import school.hei.patrimoine.cas.example.BakoCas;
+import school.hei.patrimoine.modele.Personne;
 
 public class CasSetSupplier implements Supplier<CasSet> {
   @Override
   public CasSet get() {
-    throw new RuntimeException("Implement it");
+    var mardi8avril2025 = LocalDate.of(2025, 4, 8);
+    var bakoCas =
+        new BakoCas(mardi8avril2025, mardi8avril2025.with(lastDayOfYear()), new Personne("Bako"));
+
+    return new CasSet(Set.of(bakoCas), ariary(13_111_657));
   }
 }
